03648bb2b1c4606a4ab5d892f8ccee1fe8d03cb7
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2023-02-17 22:42:50 +0530

    Working Commit

--- popup/popup.js
+++ popup/popup.js
@@ -1,38 +1,29 @@
 
 
 async function connectHandler() {
-    
-    let people = document.getElementsByClassName('reusable-search__result-container');
+    console.log('hey');
+    let people = document.getElementsByClassName('entity-result__actions entity-result__divider');
     for(let person of people){
-        let personObj = person.querySelector('.entity-result__actions entity-result__divider');
+        let personObj = person.getElementsByTagName('button')[0];
+        
         if(personObj.innerText === "Connect"){
             
-            personObj.getElementsByClassName('artdeco-button artdeco-button--2 artdeco-button--secondary ember-view')[0].click();
-            await new Promise(r => setTimeout(r, Math.floor(Math.random() * (10 - 5 + 1) + 5)));
+            personObj.click();
+            await new Promise(r => setTimeout(r, Math.floor(5000)));
 
             document.querySelector('.send-invite button.artdeco-button--primary').click()
-            await new Promise(r => setTimeout(r, 1000))
+            await new Promise(r => setTimeout(r, 2000));
             
-        };
-    }
-}
-async function injectScript() {
-    
-    if(document.querySelector('.hovbutton').textContent === "Start Connect") {
-        document.querySelector('.hovbutton').textContent = "Stop Connect";
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            chrome.scripting.executeScript({target: {tabId: tabs[0].id}, func: connectHandler
-            })})
         }
-    else {
-        document.querySelector('.hovbutton').textContent = "Start Connect";
-        console.log("RESET");
-        return;
     }
-
 }
-chrome.storage.local.set({ connectBtn: 0 }).then(() => {
-    console.log("Value is set to " + value);
-  });
-document.querySelector('.hovbutton').addEventListener('click', injectScript)
+
+
+document.querySelector('.hovbutton').addEventListener('click', () => {
+    
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        chrome.scripting.executeScript({target: {tabId: tabs[0].id}, func: connectHandler
+        })})
+
+});
 
