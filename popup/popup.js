c8785e573868547675daa67d25b88cef2dd63c3d
Rohan Sirohia <52482556+RohanTrix@users.noreply.github.com>
2023-02-18 12:24:15 +0530

    Changes

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,9 @@
-
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset;
+  }
 
 async function connectHandler() {
-    console.log('hey');
     let people = document.getElementsByClassName('entity-result__actions entity-result__divider');
     for(let person of people){
         let personObj = person.getElementsByTagName('button')[0];
@@ -9,21 +11,37 @@ async function connectHandler() {
         if(personObj.innerText === "Connect"){
             
             personObj.click();
-            await new Promise(r => setTimeout(r, Math.floor(5000)));
+            let randomNum = Math.floor(Math.random() * 6) + 5;
+            await new Promise(r => setTimeout(r, randomNum * 1000));
 
             document.querySelector('.send-invite button.artdeco-button--primary').click()
             await new Promise(r => setTimeout(r, 2000));
-            
+
+            chrome.runtime.sendMessage({connectSuccess: "true"});
         }
     }
 }
 
-
-document.querySelector('.hovbutton').addEventListener('click', () => {
+document.querySelector('.hovbutton').addEventListener('click', async () => {
     
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         chrome.scripting.executeScript({target: {tabId: tabs[0].id}, func: connectHandler
         })})
-
 });
 
+let connectCnt = 0;
+var circle = document.querySelector('circle');
+var radius = circle.r.baseVal.value;
+var circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
+setProgress(0);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
+{
+    connectCnt++;
+    document.querySelector('#connectCnt').textContent = `Connections Sent: ${connectCnt}`; 
+    let progVal = (connectCnt/10)*100
+    setProgress(progVal);
+})
